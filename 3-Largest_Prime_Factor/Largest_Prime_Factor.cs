6bb4c2a9b9ee2ada45f3ce55f37760456ef8c4c3
burakibrahim@gmail.com <burakibrahim@gmail.com>
2021-01-08 19:49:58 +0300

    Names are fÅixed

--- 3-Largest_Prime_Factor/Largest_Prime_Factor.cs
+++ 3-Largest_Prime_Factor/Largest_Prime_Factor.cs
@@ -1,33 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace Rextester
+namespace _3_Prime_Factor
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // List<int> numbers = new List<int>();
-            /*The prime factors of 13195 are 5, 7, 13 and 29.what is the largest prime factor of the number 600851475143 ?*/
-            double max=0;
-            for(double i=2;i<600851475143;i++){
-                if(600851475143%i==0){
-                    bool asalmi=true;     
-                    for(double j=2;j<i;j++){
-                        if(i%j==0){
-                            asalmi=false;
+            /// Asal Bölenler
+            long max = 0;
+            long number = 13195; //600851475143 
+            bool isitPrimeFactor;
+            for (long i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isitPrimeFactor = true;
+                    for (long j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isitPrimeFactor = false;
                             break;
                         }
                     }
-                    if(asalmi){
-                        if(max < i){
+                    if (isitPrimeFactor)
+                    {
+                        if (max < i)
+                        {
                             max = i;
                         }
-                        //Console.WriteLine(i+"\t");                      
+                        //Console.WriteLine(i);
                     }
                 }
             }
-            Console.WriteLine(max+"\t");       
+            Console.WriteLine("Max prime factor is {0}",max);
+            Console.ReadKey();
         }
     }
 }
