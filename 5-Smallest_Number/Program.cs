19e1ffc94c1b7a56c35f5ff14e9d3a756e1ad8e4
burakibrahim@gmail.com <burakibrahim@gmail.com>
2021-01-12 14:42:09 +0300

    Project Euler 5 Smallest Number Solution Fixed

--- 5-Smallest_Number/Program.cs
+++ 5-Smallest_Number/Program.cs
@@ -15,21 +15,23 @@ namespace _5_Smallest_Number
             What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?*/
             // 2520 - 232792560
             int limit = 20, counter = 2;
-            bool interrupt;
+            bool interrupt = true;
             for (int i = 2; i < 1000000000; i++)
             {
-                interrupt = true;
-                while (i%counter==0 && counter <= limit)
+                if (interrupt == false) // if counter = limit  break for loop
                 {
-                    interrupt = !interrupt;
+                    break;
+                }
+                while (i % counter == 0 && counter <= limit) // if there is no remainder,it mean i can divide  counter variable and print i
+                {
                     if (counter == limit)
                     {
                         Console.WriteLine(i);
+                        interrupt = !interrupt;
                     }
                     counter++;
                 }
                 counter = 2;
-
             }
             Console.ReadKey();
         }
