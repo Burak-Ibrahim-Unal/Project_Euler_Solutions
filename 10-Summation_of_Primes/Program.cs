2f396be8c94d5c5eed587e020615f528a5c8a193
burakibrahim@gmail.com <burakibrahim@gmail.com>
2021-01-15 19:00:53 +0300

    Solution 10 Summation of primes fixed

--- 10-Summation_of_Primes/Program.cs
+++ 10-Summation_of_Primes/Program.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     class Program
-    {
+
+
         static void Main(string[] args)
         {
           /* The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
             Find the sum of all the primes below two million.*/
+            //142913828922
             long number = 2000000;
             List<long> primeList = new List<long>();
             primeList.Add(2);
